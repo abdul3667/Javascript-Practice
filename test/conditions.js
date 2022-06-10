5bf9f2d3f5d3d29c4a25cbf715c63d1cb566587e
abdul3667 <96430324+abdul3667@users.noreply.github.com>
2022-06-10 04:21:09 -0700

    conditional statements

---
+++ test/conditions.js
@@ -0,0 +1,48 @@
+const age = 17
+const doesdrive =false;
+
+
+if (doesdrive){
+    console.log( " you can drive");
+
+}
+
+else {
+    console.log( "you cannot drive");
+}
+
+
+// &&(and operator)
+// || (or operator)
+
+
+
+
+//ternirary operator
+
+console.log(age === 45? 'age is 45': 'age is not 45')
+
+
+
+
+
+
+// switch statements
+
+
+switch (age){
+    case 18:
+        console.log('you are 18');
+        break // does not moves ahead to the second condition
+    case 17:
+        console.log('you are 17');
+        break // does not moves ahead to the second condition
+    default:
+        console.log("sorry");
+        break
+        
+        
+        
+
+
+}
